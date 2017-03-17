2d22687ca0d73800c5c20ab211dfbdc73e3d1a2b
zb <zb@163.com>
2017-03-17 22:20:59 +0800

    增加文件

---
+++ YMZP/YMZP/person.h
@@ -0,0 +1,13 @@
+//
+//  person.h
+//  YMZP
+//
+//  Created by zhangbin on 2017/3/17.
+//  Copyright © 2017年 zhangbin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface person : NSObject
+
+@end
---
+++ YMZP/YMZP/person.m
@@ -0,0 +1,13 @@
+//
+//  person.m
+//  YMZP
+//
+//  Created by zhangbin on 2017/3/17.
+//  Copyright © 2017年 zhangbin. All rights reserved.
+//
+
+#import "person.h"
+
+@implementation person
+
+@end
